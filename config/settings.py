949caebe3842a22f79410de76b8bfbb8b77d98ee
rodennis <rev.rltodd@gmail.com>
2022-02-01 21:30:46 -0600

    updated procfile name

--- config/settings.py
+++ config/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-mb4#we28^@qxwfv%-*nt6g5y!r2*qw(csg+pk)@492h#qf4uy$'
